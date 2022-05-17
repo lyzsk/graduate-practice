03f2b1380607bb886e7cd0bee825578e9f7ee3d4
lyzsk <sichu.huang2021@outlook.com>
2022-05-17 03:13:51 +0100

    2022/05/17

---
+++ src/main/java/cn/sichu/graduate_practice/dfs/SurroundedRegions.java
@@ -0,0 +1,45 @@
+package cn.sichu.graduate_practice.dfs;
+
+/**
+ * https://leetcode.cn/problems/surrounded-regions/
+ * 
+ * @author sichu
+ * @date 2022/05/17
+ */
+public class SurroundedRegions {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                boolean isEdge = i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    dfs(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private static void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1 || board[i][j] == '#'
+            || board[i][j] == 'X') {
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+}
